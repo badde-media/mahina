4c2907963164c0fa109fe9a63d38e8eba24d3df5
Daniel Badde <gitkraken@badde.media>
2020-06-08 01:23:40 +0200

    Search
    test br

--- template-parts/modal-search.php
+++ template-parts/modal-search.php
@@ -21,7 +21,7 @@
 				)
 			);
 			?>
-
+<br />
 			<button class="toggle search-untoggle close-search-toggle fill-children-current-color" data-toggle-target=".search-modal" data-toggle-body-class="showing-search-modal" data-set-focus=".search-modal .search-field" aria-expanded="false">
 				<span class="screen-reader-text"><?php _e( 'Close search', 'twentytwenty' ); ?></span>
 				<?php twentytwenty_the_theme_svg( 'cross' ); ?>
