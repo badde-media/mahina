b73fc7428aafb51ef18fc7dab06081d138d07a04
Daniel Badde <gitkraken@badde.media>
2020-06-08 01:28:08 +0200

    Search
    flex-wrap

--- searchform.php
+++ searchform.php
@@ -23,6 +23,6 @@ $twentytwenty_aria_label = ! empty( $args['label'] ) ? 'aria-label="' . esc_attr
 	<label for="<?php echo esc_attr( $twentytwenty_unique_id ); ?>">
 		<span class="screen-reader-text"><?php _e( 'Search for:', 'twentytwenty' ); // phpcs:ignore: WordPress.Security.EscapeOutput.UnsafePrintingFunction -- core trusts translations ?></span>
 		<input type="search" id="<?php echo esc_attr( $twentytwenty_unique_id ); ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'twentytwenty' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-	</label><br />
+	</label>
 	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'twentytwenty' ); ?>" />
 </form>
